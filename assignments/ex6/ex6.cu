f4c545dcbe2abf2ee9a7047a89588716fe80ced5
Davide Scassola <projanko@gmail.com>
2019-05-11 11:50:31 +0200

    ex6

--- assignments/ex6/ex6.cu
+++ assignments/ex6/ex6.cu
@@ -2,91 +2,92 @@
 #include <math.h>
 
 #define N 8192
-#define THREAD_PER_BLOCK_SIDE 2
-
-/*
-__global__ void multiply(int * in1, int * in2, int * out, int size)
-{
-    int index = threadIdx.x + blockIdx.x * blockDim.x;
-
-    int startrow = (index / size) * size;
-    int startcol = index % size;
-    int i;
-    int sum = 0;
-    for(i = 0; i < size; ++i)
-        sum += in1[startrow + i] * in2[startcol + i * size];
-
-    out[index] = sum;
-}
-*/
+#define THREAD_PER_BLOCK_SIDE 32
 
 __global__ void transpose(int * in, int * out, int size)
 {
+    int temp_side = blockDim.x;
+    __shared__ int temp_matrix1[temp_side*temp_side];
+    __shared__ int temp_matrix2[temp_side*temp_side];
+
+    int temp_i = threadIdx.y*temp_side + threadIdx.x;
+    int temp_i_t = threadIdx.x*temp_side + threadIdx.y;
+    int global_i = blockIdx.y*blockDim.y*size + blockIdx.x*blockDim.x + threadIdx.y*size + threadIdx.x;
+    int global_i_t = blockIdx.x*blockDim.y*size + blockIdx.y*blockDim.x + threadIdx.y*size + threadIdx.x;
+
+    // copy submatrix in shared memory
+    temp_matrix1[temp_i] = in[global_i_t];
+
+    // transpose submatrix in shared memory
+    temp_matrix2[temp_i] = temp_matrix1[temp_i_t];
+
+    // copy submatrix in main memory
+    out[global_i] = temp_matrix2[temp_i];
 
 }
 
 int main()
 {
 
-	int * h_in, * h_out;
+    int * h_in, * h_out;
     int * d_in, * d_out;
     int size = N*N;
     int size_in_memory = size * sizeof(int);
     int i;
 
 
-	// timing
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop)
+    // timing
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
 
 
-	//allocate memory in host and device
-	h_in = (int *)malloc(size_in_memory);
-	h_out = (int *)malloc(size_in_memory);
+    //allocate memory in host and device
+    h_in = (int *)malloc(size_in_memory);
+    h_out = (int *)malloc(size_in_memory);
 
-	cudaMalloc((void**)&d_in, size_in_memory);
+    cudaMalloc((void**)&d_in, size_in_memory);
     cudaMalloc((void**)&d_out, size_in_memory);
 
 
-	//fill matrix in host
+    //fill matrix in host
     for(i = 0; i<size; i++)
-		in1[i] = i;
+        h_in[i] = i;
 
 
-	//transfer matrix from host to device
-	cudaMemcpy(d_in, h_in, size_in_memory, cudaMemcpyHostToDevice);
+    //transfer matrix from host to device
+    cudaMemcpy(d_in, h_in, size_in_memory, cudaMemcpyHostToDevice);
 
 
-	//transpose matrix in device
+    //transpose matrix in device
     dim3 grid, block;
     block.x = THREAD_PER_BLOCK_SIDE;
     block.y = THREAD_PER_BLOCK_SIDE;
     grid.x = N / block.x;
     grid.y = N / block.y;
 
-	cudaEventRecord(start);
+    cudaEventRecord(start);
     transpose<<< grid, block >>>(d_in, d_out, N);
-	cudaEventRecord(stop);
+    cudaEventRecord(stop);
 
 
-	//transfer matrix in host
-	cudaMemcpy(h_out, d_out, size_in_memory, cudaMemcpyDeviceToHost);
+    //transfer matrix in host
+    cudaMemcpy(h_out, d_out, size_in_memory, cudaMemcpyDeviceToHost);
 
 
-	//free memory   
+    //free memory   
     free(h_in);
-	free(h_out);
+    free(h_out);
     cudaFree(d_in);
-	cudaFree(d_out);
+    cudaFree(d_out);
 
 
-	//showing Bandwidth
-	cudaEventSynchronize(stop);
-	float milliseconds = 0;
-	cudaEventElapsedTime(&milliseconds, start, stop);
+    //showing Bandwidth
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
 
-	printf("Effective Bandwidth (GB/s): %fn", size_in_memory/milliseconds/1e6);
+    printf("\nEffective Bandwidth (GB/s): %f \n", size_in_memory/milliseconds/1e6);
 
 
     return 0;
