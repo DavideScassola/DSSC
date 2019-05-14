558e333f567f6d597dcc6d7128b975de5480bb18
Davide Scassola <dscassol@login2.(none)>
2019-05-14 14:26:38 +0200

    added results

---
+++ assignments/ex6/test.sh
@@ -0,0 +1,26 @@
+cd /home/dscassol/parallel_programming/DSSC/assignments/ex6
+
+module load cudatoolkit
+
+printf "\n\n\n" >> results.txt
+echo "./fast_transpose_int.x" >> results.txt
+./fast_transpose_int.x >> results.txt
+
+printf "\n\n\n" >> results.txt
+echo "./fast_transpose_double_64.x" >> results.txt
+./fast_transpose_double_64.x >> results.txt
+
+printf "\n\n\n" >> results.txt
+echo "./fast_transpose_double_512.x" >> results.txt
+./fast_transpose_double_512.x >> results.txt
+
+printf "\n\n\n" >> results.txt
+echo "./fast_transpose_double_1024.x">> results.txt
+./fast_transpose_double_1024.x >> results.txt
+
+printf "\n\n\n" >> results.txt
+echo "./fast_transpose_double_best.x">> results.txt
+./fast_transpose_double_best.x >> results.txt
+
+rm test.sh.*
+exit
